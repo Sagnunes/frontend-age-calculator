8c9c40e097cb19adf0904c222aed69d71fcca4a1
Sérgio Nunes <sagnunes@gmail.com>
2024-03-12 08:50:53 +0000

    mobile layout

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily:{
+        poppins:['Poppins','sans-serif']
+      }
+    },
+    colors:{
+      lightGrey:"hsl(0, 0%, 86%)",
+      white:"hsl(0, 0%, 100%)",
+      purple:"hsl(259, 100%, 65%)",
+      smokeyGrey:"hsl(0, 1%, 44%)",
+      offBack:"hsl(0, 0%, 8%)",
+      lightRed:'hsl(0, 100%, 67%)'
+    }
+  },
+  plugins: [],
+}
